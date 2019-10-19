26e286ec03dab9eba1970db3c952288c95c22b9c
manager <manager.itsource>
2019-10-19 20:14:08 +0800

    商品模块

--- aigou-web-admin/src/views/product/Product.vue
+++ aigou-web-admin/src/views/product/Product.vue
@@ -36,9 +36,9 @@
 			</el-table-column>
 			<el-table-column prop="subName" label="副标题" width="120" sortable>
 			</el-table-column>
-			<el-table-column prop="productType.name" label="商品类型" width="100" sortable>
+			<el-table-column prop="productType.name" label="商品类型" width="120" sortable>
 			</el-table-column>
-			<el-table-column prop="brand.name" label="品牌" width="100" sortable>
+			<el-table-column prop="brand.name" label="品牌" width="90" sortable>
 			</el-table-column>
 			<el-table-column prop="onSaleTime" label="上架时间" width="140" sortable
 			:formatter = "formonSaleTime">
@@ -189,11 +189,49 @@
 		</el-dialog>
 
 		<el-dialog title="显示属性" v-model="handleViewFormVisible" :close-on-click-modal="false">
-			<el-form :model="handleViewForm" label-width="80px" :rules="handleViewFormRules" ref="handleViewForm">
+			<el-form label-width="80px" style="margin-left: -20px">
+				<el-form-item v-for="viewProperty in viewProperties">
+					<el-row>
+						<el-col :span="4">
+							{{viewProperty.specName}}：
+						</el-col>
+						<el-col :span="20">
+							<el-input v-model="viewProperty.value" auto-complete="off"></el-input>
+						</el-col>
+					</el-row>
+				</el-form-item>
 			</el-form>
 			<div slot="footer" class="dialog-footer">
 				<el-button @click.native="handleViewFormVisible = false">取消</el-button>
 				<el-button type="primary" @click.native="handleViewSubmit" :loading="addLoading">提交</el-button>
+			</div>
+		</el-dialog>
+
+
+		<el-dialog title="SKU属性" v-model="handleSkuFormVisible" :close-on-click-modal="false">
+			<!--(SkuProperty,i)  在for循环的同时，可以把当前数据的下标循环出来-->
+			<el-card class="box-card" v-for="(SkuProperty,i) in SkuProperties">
+				<div slot="header" class="clearfix">
+					<span style="line-height: 36px;">{{SkuProperty.specName}}</span>
+				</div>
+				<div v-for="index in SkuProperty.options.length + 1 " class="text item">
+					<el-row>
+						<el-col :span="20">
+							<el-input v-model="SkuProperty.options[index - 1]" auto-complete="off"></el-input>
+						</el-col>
+						<el-col :span="4">
+							<el-button type="primary" icon="delete" @click="removeProperTy(i,index-1)"></el-button>
+						</el-col>
+					</el-row>
+				</div>
+			</el-card>
+			<el-table :data="skus"style="width: 100%;">
+				<el-table-column v-for="(value,key) in skus[0]" :label="key" :prop="key">
+				</el-table-column>
+			</el-table>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="handleSkuFormVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="handleSkuSubmit" :loading="addLoading">提交</el-button>
 			</div>
 		</el-dialog>
 
@@ -209,6 +247,14 @@
 	export default {
 		data() {
 			return {
+
+			    //SKU弹窗里面的table表格
+			    skus:[],
+
+                SkuProperties:[],
+			    //SKU属性的弹窗
+                handleSkuFormVisible:false,
+                viewProperties:[],
 			    //显示属性弹窗
                 handleViewFormVisible:false,
 			    //存放编辑页面新上传的图片；
@@ -282,6 +328,40 @@
 			}
 		},
 		methods: {
+            removeProperTy(index1,index2){
+                //index1 第几个SKU属性
+				//index 当前SKU属性的第几个值
+				//这句话的解析：找到第index1的SkuProperties下的SKU属性下的options，并删除options数组里面下标是index2长度为1的数据
+                this.SkuProperties[index1].options.splice(index2,1);
+			},
+		    /*商品的显示属性保存*/
+            handleViewSubmit(){
+                let productId = this.sels[0].id;
+                this.$confirm('确认保存吗？', '提示', {}).then(() => {
+                    this.addLoading = true;
+                    //NProgress.start();
+                    this.$http.post("/product/product/saveViewProperties?productId="+productId,this.viewProperties).then(res=>{
+                        let {success,message,restObj} = res.data;
+                        if(success){
+                            this.addLoading = false;
+                            //NProgress.done();
+                            this.$message({
+                                message: '操作成功',
+                                type: 'success'
+                            });
+                            this.handleViewFormVisible = false;
+                            this.viewProperties=[];
+                        }else{
+                            this.$message({
+                                message: '操作失败',
+                                type: 'error'
+                            });
+                        }
+                    }).catch({
+
+                    });
+                });
+			},
             /*编辑室获取回填菜单*/
             loadGetPath(index, row){
                 this.$http.get("/product/productType/"+row.productTypeId).then(res=>{
@@ -371,10 +451,47 @@
                 return data;
             },
             handleViewProperties(){
+                if (this.sels.length == 0){
+                    this.$message({
+                        message: '请选择数据',
+                        type: 'warning'
+                    });
+                    return;
+				}
+                if (this.sels.length >1){
+                    this.$message({
+                        message: '只能选择一行数据',
+                        type: 'warning'
+                    });
+                    return;
+                }
+                let productId = this.sels[0].id;
+                this.$http.get("/product/product/getviewProperties/"+productId).then(res=>{
+                    console.debug(res.data);
+                    this.viewProperties = res.data;
+				}).catch({});
                 this.handleViewFormVisible = true;
 			},
             handleSkuProperties(){
-                alert("SKU属性");
+                if (this.sels.length == 0){
+                    this.$message({
+                        message: '请选择数据',
+                        type: 'warning'
+                    });
+                    return;
+                }
+                if (this.sels.length >1){
+                    this.$message({
+                        message: '只能选择一行数据',
+                        type: 'warning'
+                    });
+                    return;
+                }
+                let productId = this.sels[0].id;
+                this.$http.get("/product/product/getskuProperties/"+productId).then(res=>{
+                    this.SkuProperties = res.data;
+                }).catch({});
+                this.handleSkuFormVisible = true;
             },
             handleOnSale(){
                 alert("上架");
@@ -625,6 +742,36 @@
 			this.getProduct();
 			this.loadTypeTree();
 			this.getBrands();
+		},
+		//watch是ES6的一个监听器
+		watch:{
+		  SkuProperties:{
+		   handler(val,oldval){
+		       //filter是一个过滤器，过滤掉sku里面的options为空的数组
+		     let skuPropertiesArr = this.SkuProperties.filter(e=>e.options.length>0);
+		     //es6的reduce方法 currentIndex 当前元素的索引
+		     let result = skuPropertiesArr.reduce((pre,cur,currentIndex)=>{
+		         //最开始：pre 是一个空的数组
+				 //   cur={"specName":"年龄",options:["御姐","萝莉"]}
+		         let map =[];
+				 pre.forEach(e1=>{ //第一次循环：e1：{} 第二次循环 e1：{"年龄":"御姐"}
+				     cur.options.forEach(e2=>{//e2:御姐 萝莉
+				         let obj = Object.assign({},e1); //循环1,obj:{}
+				         obj[cur.specName] = e2;//循环1：obj{"年龄":"御姐"}
+						 //判断是不是最后的一次reduce
+						 if (currentIndex == skuPropertiesArr.length -1 ){
+						     obj.price = 0;
+						     obj.store = 0;
+						 }
+				         map.push(obj); //map ：{"年龄":"御姐"}
+					 })
+				 })
+				 return map;
+			 },[{}])
+			  this.skus = result;
+		   },
+			deep:true
+			}
 		}
 	}
 
